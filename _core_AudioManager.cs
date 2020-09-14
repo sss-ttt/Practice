ab67fad0fafe1e153c753001716998bd92d26737
sss-ttt <71086437+sss-ttt@users.noreply.github.com>
2020-09-14 14:19:06 +0900

    UnityAudioManager
    超簡易ランダムサウンド再生

---
+++ _core_AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class _core_AudioManager : MonoBehaviour
+{
+    static public _core_AudioManager AudioManager;
+    public static AudioSource[] AudioSources;
+    void Awake()
+    {
+        if (AudioManager == null)
+        {
+            AudioManager = this;
+            DontDestroyOnLoad(gameObject);
+            AudioSources = GetComponents<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public List<data_audiopack> audiopacks;
+    public void PlaySound(string AudioPackTag)
+    {
+        foreach (data_audiopack audiopack in audiopacks)
+        {
+            if (audiopack.NameTag == AudioPackTag)
+            {
+                AudioSources[0].PlayOneShot(audiopack.clips[Random.Range(0, audiopack.clips.Length)]);
+                return;
+            }
+        }
+    }
+}
---
+++ data_audiopack.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+[CreateAssetMenu(menuName = "Create AudioPackData")]
+[System.Serializable]
+public class data_audiopack : ScriptableObject
+{
+    public string NameTag; 
+    public AudioClip[] clips;
+
+}
+
